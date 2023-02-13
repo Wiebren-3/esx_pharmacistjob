c7eac0dae780cf6bfef9f65852302c1157f0fba0
Wiebren <114926599+Wiebren-3@users.noreply.github.com>
2023-02-13 13:08:59 +0100

    Create en.lua

---
+++ locales/en.lua
@@ -0,0 +1,63 @@
+Locales['en'] = {
+  --######################
+  --####MENU ITEMS########
+  --######################
+  ['loanMenuItem']                                    = "📁 Loans",
+  ['activeLoanMenuItem']                              = "📁 Active Loans",
+  ['frozenLoanMenuItem']                              = "📁 Frozen Loans",
+  ['doLoanMenuItem']                                  = "💰 Make a Loan",
+  ['billingMenuItem']                                 = "📝 Billing",
+  ['separatorMenuItem']                               = "- - - - -",
+
+
+  --######################
+  --####MENU NAME#########
+  --######################
+  ['pharmacist']                                            = '💊 - Pharmacy',
+
+
+  --##############################
+  --#########TABLE ITEMS##########
+  --##############################
+  ['client']                                          = 'Client',
+  ['rate']                                            = 'Rate',
+  ['pharmacist']                                      = 'Employe',
+  ['actions']                                         = 'Actions',
+  ['status']                                          = 'Status',
+  --######################
+  --#####MENU Actions#####
+  --######################
+  ['amount']                                          = 'Amount',
+  ['invalid_amount']                                  = '❌ Invalid amount (1 - 10 000 000)',
+  ['invalid_rate']                                    = '❌ Invalid rate (1 - 250)',
+  ['invalid_number']                                  = '❌ Invalid number (1 - 20)',  
+  ['no_player']                                       = '❌ No one around you',
+
+  --######################
+  --#########CARS#########
+  --######################
+  ['press_input_context_to_park_car']                 = 'Press [E] to park the vehicle',
+  ['press_input_context_to_spawn_car']                = '[E] Company car - Pharmacy',
+  ['garage']                                          = 'Garage - Pharmacy',
+  ['blocked_spawn_point']                             = '❌ The parking space is encumbered',
+  ['not_boss']                                        = "❌ You are not a Manager.",
+  ['not_in_auth_car']                                 = "❌ You are not in an authorized company vehicle.",
+
+  --######################
+  --########PHONE#########
+  --######################
+  ['phone_receive']                                   = 'Citizen',
+  ['phone_label']                                     = 'pharmacist',
+  --######################
+  --#######STOCKS#########
+  --######################
+  ['get_weapon']                                         = '🔫 Take weapon',
+  ['put_weapon']                                         = '🔫 Deposit weapon',
+  ['take_inventory']                                     = '📄 Take an item',
+  ['put_inventory']                                      = '📄 Deposit an item',
+  ['take_weapon_action']                                 = "✔️ Armory - Take weapon",
+  ['put_weapon_action']                                  = "❌ Armory - Deposit weapon",
+  ['quantity']                                           = "🔢 Quantity",
+  ['quantity_invalid']                                   = "❌ Invalid quantity !",
+  ['inventory']                                          = "🎒 Backpack",
+}
