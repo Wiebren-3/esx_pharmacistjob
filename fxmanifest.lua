0c7e3cba67c0fd4bee4ff075809b473d02144466
Wiebren <114926599+Wiebren-3@users.noreply.github.com>
2023-02-13 13:34:03 +0100

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,32 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Wiebren#6393'
+description 'ESX Pharmacistjob'
+version '1.0.0'
+
+server_scripts {
+	'@async/async.lua',
+	'@mysql-async/lib/MySQL.lua',
+	'@es_extended/locale.lua',
+	'config.lua',
+	'locales/en.lua',
+	'locales/en.lua',
+	'server/main.lua'
+}
+
+client_scripts {
+	'@es_extended/locale.lua',
+	'config.lua',
+	'locales/en.lua',
+	'locales/fr.lua',
+	'client/main.lua'
+}
+
+dependencies {
+    'es_extended',
+    'async',
+	'mysql-async'
+}
+
+
