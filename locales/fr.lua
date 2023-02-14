810e254e7392a397b0c1eb4e224b70771ac873ca
Wiebren <114926599+Wiebren-3@users.noreply.github.com>
2023-02-14 11:59:49 +0100

    Create fr.lua

---
+++ locales/fr.lua
@@ -0,0 +1,62 @@
+Locales['nl'] = {
+  --######################
+  --####MENU ITEMS########
+  --######################
+  ['loanMenuItem']                                    = "📁 Lonen",
+  ['activeLoanMenuItem']                              = "📁 Actieve lonen",
+  ['frozenLoanMenuItem']                              = "📁 Bevroren lonen",
+  ['doLoanMenuItem']                                  = "📁 Creeër een loon",
+  ['billingMenuItem']                                 = "📝 Facturing",
+  ['separatorMenuItem']                               = "- - - - -",
+
+
+  --######################
+  --####MENU NAME#########
+  --######################
+  ['pharmacist']                                            = '💊 - Apotheek',
+
+
+  --##############################
+  --#########TABLE ITEMS##########
+  --##############################
+  ['client']                                          = 'Client',
+  ['rate']                                            = 'Rate',
+  ['pharmacist']                                      = 'Medewerker',
+  ['actions']                                         = 'Acties',
+  ['status']                                          = 'Status',
+  --######################
+  --#####MENU Actions#####
+  --######################
+  ['amount']                                          = 'Quantité',
+  ['invalid_amount']                                  = '❌ Montant incorrect (1 - 10 000 000)',
+  ['invalid_rate']                                    = '❌ Taux incorrect (1 - 250)',
+  ['invalid_number']                                  = '❌ Numéro incorrect (1 - 20)',  
+  ['no_player']                                       = '❌ Il ny a personne près de chez vous.',
+
+  --######################
+  --#########CARS#########
+  --######################
+  ['press_input_context_to_park_car']                 = 'Appuyez sur [E] pour garer le véhicule.',
+  ['press_input_context_to_spawn_car']                = 'Appuyez sur [E] pour sortir un véhicule de travail du garage.',
+  ['garage']                                          = 'Garage - Pharmacie',
+  ['blocked_spawn_point']                             = '❌ Un objet vous empêche de sortir du garage en voiture.',
+  ['not_boss']                                        = "❌ Vous n'êtes pas assez haut dans la hiérarchie",
+  ['not_in_auth_car']                                 = "❌ Vous n'avez accès à aucun de ces véhicules.",
+
+  --######################
+  --########PHONE#########
+  --######################
+  ['phone_receive']                                   = 'Civil',
+  ['phone_label']                                     = 'Pharmacien',
+  --######################
+  --#######STOCKS#########
+  --######################
+  ['get_weapon']                                         = '📄 Prenez une arme dans le stockage.',
+  ['put_weapon']                                         = '📄 Mettez une arme en réserve.',
+  ['take_inventory']                                     = '📄 Prenez un objet du stockage.',
+  ['put_inventory']                                      = '📄 Mettez un objet en réserve.',
+  ['take_weapon_action']                                 = "📄 Stockage - Saisissez l'arme.",
+  ['put_weapon_action']                                  = "📄 Stockage - Pose d'arme.",
+  ['quantity']                                           = "📄 Quantité",
+  ['quantity_invalid']                                   = "📄 Montant incorrect!",
+  ['inventory']                                          = "🎒 Inventaire",
