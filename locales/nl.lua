c0f57d7fc3e1779e2af0af9e221bbb625e99982a
Wiebren <114926599+Wiebren-3@users.noreply.github.com>
2023-02-14 11:36:12 +0100

    Create nl.lua

---
+++ locales/nl.lua
@@ -0,0 +1,63 @@
+Locales['en'] = {
+  --######################
+  --####MENU ITEMS########
+  --######################
+  ['loanMenuItem']                                    = "📁 Loans",
+  ['activeLoanMenuItem']                              = "📁 Active Loans",
+  ['frozenLoanMenuItem']                              = "📁 Frozen Loans",
+  ['doLoanMenuItem']                                  = "📁 Make a Loan",
+  ['billingMenuItem']                                 = "📝 Facturing",
+  ['separatorMenuItem']                               = "- - - - -",
+
+
+  --######################
+  --####MENU NAME#########
+  --######################
+  ['pharmacist']                                            = '💊 - Apotheek',
+
+
+  --##############################
+  --#########TABLE ITEMS##########
+  --##############################
+  ['client']                                          = 'Client',
+  ['rate']                                            = 'Rate',
+  ['pharmacist']                                      = 'Medewerker',
+  ['actions']                                         = 'Acties',
+  ['status']                                          = 'Status',
+  --######################
+  --#####MENU Actions#####
+  --######################
+  ['amount']                                          = 'Amount',
+  ['invalid_amount']                                  = '❌ Onjuiste hoeveelheid (1 - 10 000 000)',
+  ['invalid_rate']                                    = '❌ Onjuiste tarief (1 - 250)',
+  ['invalid_number']                                  = '❌ Onjuist nummer (1 - 20)',  
+  ['no_player']                                       = '❌ Er is niemand bij je in de buurt.',
+
+  --######################
+  --#########CARS#########
+  --######################
+  ['press_input_context_to_park_car']                 = 'Druk op [E] om het voertuig te parkeren.',
+  ['press_input_context_to_spawn_car']                = 'Druk op [E] om een werkvoertuig uit de garage te halen.',
+  ['garage']                                          = 'Garage - Apotheek',
+  ['blocked_spawn_point']                             = '❌ Een object blokkeerd je om uit de garage te rijden.',
+  ['not_boss']                                        = "❌ Je bent niet hoog genoeg in ranghirarchie",
+  ['not_in_auth_car']                                 = "❌ Je hebt geen toegang tot een van deze voertuigen.",
+
+  --######################
+  --########PHONE#########
+  --######################
+  ['phone_receive']                                   = 'Burger',
+  ['phone_label']                                     = 'Apotheker',
+  --######################
+  --#######STOCKS#########
+  --######################
+  ['get_weapon']                                         = '📄 Pak een wapen uit de opslag.',
+  ['put_weapon']                                         = '📄 Leg een wapen weg in de opslag.',
+  ['take_inventory']                                     = '📄 Pak een object uit de opslag.',
+  ['put_inventory']                                      = '📄 Leg een object in de opslag.',
+  ['take_weapon_action']                                 = "📄 Opslag - Wapen pakken.",
+  ['put_weapon_action']                                  = "📄 Opslag - Wapen leggen.",
+  ['quantity']                                           = "📄 Hoeveelheid",
+  ['quantity_invalid']                                   = "📄 Onjuiste hoeveelheid!",
+  ['inventory']                                          = "🎒 Inventaris",
+}
