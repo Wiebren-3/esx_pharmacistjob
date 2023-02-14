f74b3d77b1d4a5906db93cd72ceaa909e3c0d96c
Wiebren <114926599+Wiebren-3@users.noreply.github.com>
2023-02-14 10:57:36 +0100

    Create nl_pharmasistjob.sql

---
+++ localization/nl_pharmasistjob.sql
@@ -0,0 +1,23 @@
+INSERT INTO `addon_account` (name, label, shared) VALUES
+	('society_pharmacist', 'Apotheker', 1)
+;
+
+INSERT INTO `addon_inventory` (name, label, shared) VALUES
+	('society_pharmacist', 'Apotheker', 1)
+;
+
+INSERT INTO `datastore` (name, label, shared) VALUES
+	('society_pharmacist', 'ApothekerApotheker', 1)
+;
+
+INSERT INTO `jobs` (name, label) VALUES
+	('pharmacist', 'Apotheker')
+;
+
+INSERT INTO `job_grades` (job_name, grade, name, label, salar
+	('pharmacist',0,'recruit','Stagair',60,'{}','{}'),
+	('pharmacist',1,'supplier','Leverancier',70,'{}','{}')
+	('pharmacist',2,'store_employe','Winkelmedewerker',80,'{}','{}'),
+	('pharmacist',3,'lab_employe','Labmedewerker',90,'{}','{}'),
+	('pharmacist',4,'boss','Manager',100,'{}','{}')
+;
