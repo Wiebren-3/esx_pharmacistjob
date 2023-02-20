cf5d016639df024e7814b37d3f19de7bc84ae2f4
Wiebren <114926599+Wiebren-3@users.noreply.github.com>
2023-02-20 13:16:10 +0100

    Update nl_pharmacistjob.sql

--- localization/nl_pharmacistjob.sql
+++ localization/nl_pharmacistjob.sql
@@ -1,25 +1,41 @@
 USE `es_extended`;
 
 INSERT INTO `addon_account` (name, label, shared) VALUES
-	('society_pharmacist', 'Apotheker', 1)
-;
-
-INSERT INTO `addon_inventory` (name, label, shared) VALUES
-	('society_pharmacist', 'Apotheker', 1)
+	('society_pharmacist', 'Pharmacist', 1)
 ;
 
 INSERT INTO `datastore` (name, label, shared) VALUES
-	('society_pharmacist', 'Apotheker', 1)
+	('society_pharmacist', 'Pharmacist', 1)
+;
+
+INSERT INTO `addon_inventory` (name, label, shared) VALUES
+	('society_pharmacist', 'Pharmacist', 1)
 ;
 
 INSERT INTO `jobs` (name, label) VALUES
-	('pharmacist', 'Apotheker')
+	('pharmacist', 'Pharmacist')
 ;
 
-INSERT INTO `job_grades` (job_name, grade, name, label, salar
-	('pharmacist',0,'recruit','Stagaire',60,'{}','{}'),
-	('pharmacist',1,'supplier','Leverancier',70,'{}','{}')
-	('pharmacist',2,'store_employe','Winkelmedewerker',80,'{}','{}'),
-	('pharmacist',3,'lab_employe','Labmedewerker',90,'{}','{}'),
-	('pharmacist',4,'boss','Manager',100,'{}','{}')
+INSERT INTO `job_grades` (job_name, grade, name, label, salary, skin_male, skin_female) VALUES
+	('pharmacist',0,'recruit','Stagaire',200,'{}','{}'),
+	('pharmacist',100,'supplier','Stagaire',300,'{}','{}'),
+	('pharmacist',300,'experienced_supplier','Ervaren Leverancier',350,'{}','{}'),
+	('pharmacist',500,'store_employe','Winkelmedewerker',300,'{}','{}'),
+	('pharmacist',700,'erv_mdw','Ervaren Winkelmedewerker',350,'{}','{}'),
+	('pharmacist',800,'lab_employe','Labmedewerker',400,'{}','{}'),
+	('pharmacist',800,'lab_employe','Ervaren Labmedewerker',450,'{}','{}'),
+	('pharmacist',825,'lead_suppliers','Leidinggevende Leveranciers',550,'{}','{}'),
+	('pharmacist',850,'lead_storeemployes','Leidinggevende Winkelmedewerkers',550,'{}','{}'),
+	('pharmacist',875,'lead_labemployes','Leidinggevende Labmedewerkers',550,'{}','{}'),
+	('pharmacist',900,'manager','Manager',650,'{}','{}'),
+	('pharmacist',1000,'boss','Beheerder',750,'{}','{}')
 ;
+
+CREATE TABLE `fine_types` (
+	`id` int NOT NULL AUTO_INCREMENT,
+	`label` varchar(255) DEFAULT NULL,
+	`amount` int DEFAULT NULL,
+	`category` int DEFAULT NULL,
+
+	PRIMARY KEY (`id`)
+);
